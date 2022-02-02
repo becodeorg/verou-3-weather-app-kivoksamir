bc6385204c6fa1053b3339a94fac6b8fee12c433
kivoksamir <upahmadalkordee@gmail.com>
2022-02-02 11:35:24 +0100

    change the code from been object to normal functions

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ import data from "./config.js";
 console.log(data.key);    // test
 
 
+    const apiKey= data.key;
+    
 
-const weather = {
-    apiKey: data.key,
-    fetchWeather: function (city) {
+    const fetchWeather= function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" 
         + city +
-        "&units=metric&appid="+ weather.apiKey)
+        "&units=metric&appid="+apiKey)
 
         .then((response) => {
           if (!response.ok) {   // check if the respond is not ok .. 
@@ -19,56 +19,51 @@ const weather = {
           }
           return response.json();    // convert response to json to be accessible  .. this return new promis
         })
-        .then((data) => weather.displayWeather(data)); // so we need new (then) (there are our actual data)
-    },
+        .then((data) =>displayWeather(data)); // so we need new (then) (there are our actual data)
+    };
+
 
     
-    displayWeather: function (data) {
+    const  displayWeather= function (data) {
         const { name } = data;                          //object destructuring , basic assignment
         const { icon, description } = data.weather[0];  
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         const {country}=data.sys;  // add the country code
         let country_name=country.toLowerCase(); // convert it to lowerCase
+        
 
         document.querySelector(".city").innerText = "Weather in " + name;
 
-        document.querySelector(".icon").src =
-          "http://openweathermap.org/img/w/"+icon+".png";
+        document.querySelector(".icon").src ="http://openweathermap.org/img/w/"+icon+".png";
 
         document.querySelector(".description").innerText = description;
 
         document.querySelector(".temp").innerText = temp + "°C";
 
-        document.querySelector(".humidity").innerText =
-          "Humidity: " + humidity + "%";
+        document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
 
-        document.querySelector(".wind").innerText =
-          "Wind speed: " + speed + " km/h";
+        document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
 
         document.querySelector(".country_code").innerHTML=country;
 
         
-        document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
 
       document.querySelector(".flag").src="http://openweathermap.org/images/flags/"+country_name+".png";
-     },
+     } ; 
 
-      search: function () {
-        weather.fetchWeather(document.querySelector(".search-bar").value);
-      },
-    };
+      const search = function () {
+        fetchWeather(document.querySelector(".search-bar").value);
+      };
     
-    document.querySelector(".search button").addEventListener("click",()=> {
-      weather.search();
-    });
+    
+    document.querySelector(".search button").addEventListener("click",()=> {search()});
     
     document.querySelector(".search-bar").addEventListener("keyup", (event)=> { 
-        if (event.key == "Enter") { // enter key active for search using event OR e :)
-          weather.search();
+        if (event.key == "Enter") { search();
         }
       });
     
-    weather.fetchWeather("ghent");   // call gent as standerd
+      fetchWeather("ghent");   // call gent as standerd
     
