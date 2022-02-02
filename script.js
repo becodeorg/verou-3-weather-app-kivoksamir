9e3c2404f03efb1a9dd01d8475e179807d7efc59
kivoksamir <upahmadalkordee@gmail.com>
2022-02-02 01:14:16 +0100

    add the country flag

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 import data from "./config.js";
 console.log(data.key);    // test
 
+
+
 const weather = {
     apiKey: data.key,
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" 
         + city +
-        "&units=metric&appid=" 
-        +this.apiKey)
+        "&units=metric&appid="+ weather.apiKey)
 
         .then((response) => {
           if (!response.ok) {   // check if the respond is not ok .. 
@@ -18,7 +19,7 @@ const weather = {
           }
           return response.json();    // convert response to json to be accessible  .. this return new promis
         })
-        .then((data) => this.displayWeather(data)); // so we need new (then) (there are out actual data)
+        .then((data) => weather.displayWeather(data)); // so we need new (then) (there are our actual data)
     },
 
     
@@ -27,6 +28,8 @@ const weather = {
         const { icon, description } = data.weather[0];  
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
+        const {country}=data.sys;  // add the country code
+        let country_name=country.toLowerCase(); // convert it to lowerCase
 
         document.querySelector(".city").innerText = "Weather in " + name;
 
@@ -46,10 +49,12 @@ const weather = {
         
         document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
+
+      document.querySelector(".flag").src="http://openweathermap.org/images/flags/"+country_name+".png";
      },
 
       search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
+        weather.fetchWeather(document.querySelector(".search-bar").value);
       },
     };
     
@@ -57,9 +62,7 @@ const weather = {
       weather.search();
     });
     
-    document
-      .querySelector(".search-bar")
-      .addEventListener("keyup", (event)=> { 
+    document.querySelector(".search-bar").addEventListener("keyup", (event)=> { 
         if (event.key == "Enter") { // enter key active for search using event OR e :)
           weather.search();
         }
