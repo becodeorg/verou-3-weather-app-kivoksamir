be8f507dbaa87277ce672ba30cce4f45747cbe9a
kivoksamir <upahmadalkordee@gmail.com>
2022-02-02 11:59:41 +0100

    make Module

--- script.js
+++ script.js
@@ -24,34 +24,8 @@ console.log(data.key);    // test
 
 
     
-    const  displayWeather= function (data) {
-        const { name } = data;                          //object destructuring , basic assignment
-        const { icon, description } = data.weather[0];  
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        const {country}=data.sys;  // add the country code
-        let country_name=country.toLowerCase(); // convert it to lowerCase
-        
+    import displayWeather from "./display.js"; // import , export 
 
-        document.querySelector(".city").innerText = "Weather in " + name;
-
-        document.querySelector(".icon").src ="http://openweathermap.org/img/w/"+icon+".png";
-
-        document.querySelector(".description").innerText = description;
-
-        document.querySelector(".temp").innerText = temp + "°C";
-
-        document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
-
-        document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
-
-        document.querySelector(".country_code").innerHTML=country;
-
-        
-        document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
-
-      document.querySelector(".flag").src="http://openweathermap.org/images/flags/"+country_name+".png";
-     } ; 
 
       const search = function () {
         fetchWeather(document.querySelector(".search-bar").value);
